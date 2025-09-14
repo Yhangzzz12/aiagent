9766978c1309bc3d5bcd532e8d476d1b7482eeef
YHangzzz12 <yihangfeng1030@gmail.com>
2025-09-14 04:11:19 -0700

    Chapter 2 Lesson 2 Completed

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+def get_files_info(working_directory, directory="."):
+    '''The directory parameter should be treated as a relative path within the working_directory. 
+    Use os.path.join(working_directory, directory) to create the full path, 
+    then validate it stays within the working directory boundaries.'''
+    full_path = os.path.join(working_directory, directory)
+
+
+    if directory.startswith('/') or directory.startswith('..') or (os.path.abspath(full_path).startswith(os.path.abspath(working_directory))==False):
+        return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if os.path.isfile(directory):
+        return f'   Error:{directory} is not a directory'
+
+    # means if os didn't find that specific dir in working directory
+    if not os.path.isdir(full_path):
+        return f'   Error: "{full_path}" is not a directory or you may type wrong'
+
+    
+
+
+    
+    content_of_dir = ''
+    for content in os.listdir(full_path):
+        content_of_dir += f'-{content}: file_size={os.path.getsize(os.path.join(full_path,content))} bytes, is_dir={os.path.isdir(os.path.join(full_path,content))}\n'
+    return content_of_dir
+    
+    
+
+
+    
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
 
 def main():
+    argv = sys.argv[1:]
+    verbose = "--verbose" in argv
+    if not argv:
+        print(f'the format is [uv run main.py] [text input]')
+        sys.exit(1)
+    
+    if verbose:
+        
+        print (f'User prompt: {sys.argv[1]}')
+    
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -13,10 +28,12 @@ def main():
     #Generate Contents
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    contents=messages)
     print()
     print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}')
+
+    if verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}')
     
 
 
