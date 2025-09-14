f455d4be892d81ea76f424f630e2eca95a6c6847
YHangzzz12 <yihangfeng1030@gmail.com>
2025-09-14 04:11:19 -0700

    Gemini Discord AI Bot

---
+++ tests.py
@@ -0,0 +1,18 @@
+from functions.get_files_info import get_files_info
+
+def test():
+    print('Result for current directory:')
+    print(get_files_info("calculator", "."))
+
+    print('Result for pkg directory:')
+    print(get_files_info("calculator", "pkg"))
+
+    print('Result for /bin directory:')
+    print(get_files_info("calculator", "/bin"))
+    print()
+
+    print('Result for ../ directory:')
+    print(get_files_info("calculator", "../"))
+
+
+test()
